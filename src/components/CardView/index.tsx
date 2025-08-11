079cbdf25fc124e6900b6aafe3e6260eda33edeb
Leandro Fonseca <leandromkt@outlook.com>
2025-08-11 19:01:59 -0300

    Trabalhando com tecnicas de sub componentes

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -1,13 +1,20 @@
 import { View, Text, Image } from "react-native";
 import { styles } from "./style";
 import Logo from "../../../assets/logo.png"
+import Divider from "../Divider";
 
 export default function CardView(){
-    return (
-        <View style={styles.imageContainer}>
+
+    const renderLogoBox = () => (
             <View style={styles.logoContainer}>
                 <Image style={styles.imageLogo} source={Logo} />
             </View>
+    )
+
+    return (
+        <View style={styles.imageContainer}>
+            {renderLogoBox()}
+            <Divider />
         </View>
     )
 }
