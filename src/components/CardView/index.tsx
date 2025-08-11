0ce9b2518b1226d3af0a81f71c5354ef9f0d5461
Leandro Fonseca <leandromkt@outlook.com>
2025-08-11 18:32:02 -0300

    Criando o componente de CardView

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import { styles } from "./style";
+
+export default function CardView(){
+    <View style={styles.imageContainer}>
+        <Text> CardView Works!!</Text>
+    </View>
+}
