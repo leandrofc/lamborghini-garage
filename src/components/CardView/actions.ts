881631e7642e6e39fcf6c05dc7672acd66d439ae
Leandro Fonseca <leandromkt@outlook.com>
2025-08-13 19:58:58 -0300

    implementando uma load function

--- src/components/CardView/actions.ts
+++ src/components/CardView/actions.ts
@@ -1,5 +1,15 @@
-export const loadCarData = async() => {
-    
+import { fetchGetCarData } from "../../api/getCars";
+import { CarModel } from "./props"
+
+export const loadCarData = async(
+    id: number,
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+) => {
+    try {
+        await fetchGetCarData(id, setCarData);
+    } catch (error) {
+        console.error("Erro ao buscar dados da api:", error);
+    }
 }
 
 export const handlePreviousItem = async() => {
